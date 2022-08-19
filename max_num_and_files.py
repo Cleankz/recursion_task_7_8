8a94eedb1fef0417f33ca968dab9086aef319056
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-08-19 15:42:48 +0300

    Update max_num_and_files.py

--- max_num_and_files.py
+++ max_num_and_files.py
@@ -2,8 +2,11 @@
 from genericpath import isdir
 import os.path
 
-def search(inp_list, first_max_num,idx):
+def search(inp_list, first_max_num,idx = None):
+
     second_max_num = first_max_num
+    if idx is None:
+        idx = 0
 
     if inp_list[idx] >= first_max_num:
         first_max_num = inp_list[idx]
@@ -15,11 +18,12 @@ def search(inp_list, first_max_num,idx):
 
 def find_max_num(inp_list):
 
-    first_max_num = inp_list[0]
-    second_max_num = search(inp_list,first_max_num,0)
+    idx = 0
+    second_max_num = search(inp_list,inp_list[idx])
 
     return second_max_num
 
+print(find_max_num([1,2,3,4,5,6,7,8,9]))
 
 # 8 задание:
 def show_dir_and_files(way_name):
