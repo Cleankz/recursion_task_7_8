b2dee4049c62eade759dfd781801884326a1a479
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-08-19 19:39:09 +0300

    Update max_num_and_files.py

--- max_num_and_files.py
+++ max_num_and_files.py
@@ -14,27 +14,20 @@ import os.path
 #     return search(inp_list,first_max_num,idx + 1)
 
 def find_max_num(inp_list):
-    first_max_num = inp_list[0]
-    second_max_num = 0
-    more_first_max = False
-    count_f_max_num = 0
+    first_max_num = inp_list[len(inp_list)//2]
+    second_max_num = inp_list[len(inp_list)//2]
 
-    for i in inp_list:
-        if i >= first_max_num:
-            first_max_num = i
-            count_f_max_num = count_f_max_num + 1
-        if i == first_max_num and count_f_max_num > 1:
-            more_first_max = True
-    for j in inp_list:
-            if j > second_max_num and j != first_max_num:
-                second_max_num = j
-            if more_first_max:
-                second_max_num = first_max_num
-
+    for i in range(len(inp_list)):
+        if inp_list[i] >= second_max_num and inp_list[i] != first_max_num:
+                second_max_num = inp_list[i]
+        if inp_list[i] >= first_max_num:
+            second_max_num = first_max_num
+            first_max_num = inp_list[i]
+        # if inp_list[i] <= second_max_num and inp_list[i] != first_max_num:
+        #     second_max_num = inp_list[i]
+            
 
     return second_max_num
-
-
 # 8 задание:
 def show_dir_and_files(way_name):
     print(os.listdir(way_name))
