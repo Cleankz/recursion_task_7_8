ab5e08885a314d31ef367047cc36f77d79f5e00c
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-08-20 17:07:22 +0300

    Update max_num_and_files.py

--- max_num_and_files.py
+++ max_num_and_files.py
@@ -1,4 +1,5 @@
 # 7 задание:
+from email import message
 from genericpath import isdir
 import os.path
 
@@ -15,6 +16,13 @@ def search(inp_list, first_max_num,second_max_num,idx):
     return search(inp_list,first_max_num,second_max_num,idx + 1)
 
 def find_max_num(inp_list):
+    if len(inp_list) == 0:
+        message = "list is empty!"
+        return message
+
+    if len(inp_list) ==1:
+        print("there is one element in the list: ")
+        return inp_list[0]
 
     if inp_list[0] > inp_list[1]:
         first_max_num = inp_list[0]
@@ -25,7 +33,7 @@ def find_max_num(inp_list):
     result = search(inp_list,first_max_num,second_max_num,2)
 
     return result
-
+print(find_max_num([9]))
 # 8 задание:
 def show_dir_and_files(way_name):
     print(os.listdir(way_name))
