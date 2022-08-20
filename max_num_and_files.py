d726c3a245bd49c67a2634b2029ad9b0091cb819
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-08-20 16:37:28 +0300

    Update max_num_and_files.py

--- max_num_and_files.py
+++ max_num_and_files.py
@@ -2,16 +2,18 @@
 from genericpath import isdir
 import os.path
 
-# def search(inp_list, first_max_num,idx):
-#     second_max_num = first_max_num
+def search(inp_list, first_max_num,second_max_num,idx = 2):
 
-#     if inp_list[idx] >= first_max_num:
-#         first_max_num = inp_list[idx]
+    if inp_list[idx] >= first_max_num:
+        second_max_num = first_max_num
+        first_max_num = inp_list[idx]
+    elif inp_list[idx] > second_max_num:
+        second_max_num = inp_list[idx]
+    if idx >= len(inp_list) - 1:
+        return second_max_num
 
-#     if idx == len(inp_list) - 1:
-#         return second_max_num
 
-#     return search(inp_list,first_max_num,idx + 1)
+    return search(inp_list,first_max_num,second_max_num,idx + 1)
 
 def find_max_num(inp_list):
 
@@ -21,15 +23,10 @@ def find_max_num(inp_list):
     else:
         first_max_num = inp_list[1]
         second_max_num = inp_list[0]
+    result = search(inp_list,first_max_num,second_max_num)
 
-    for i in range(2,len(inp_list)):
-        if inp_list[i] >= first_max_num:
-            second_max_num = first_max_num
-            first_max_num = inp_list[i]
-        elif inp_list[i] > second_max_num:
-            second_max_num = inp_list[i]
+    return result
 
-    return second_max_num
 # 8 задание:
 def show_dir_and_files(way_name):
     print(os.listdir(way_name))
